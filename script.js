6e5dcf4c58df073e2d287142f2616b8f24d20608
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-10-02 20:29:02 +0545

    complete: gallery page

--- script.js
+++ script.js
@@ -4,75 +4,74 @@ let projectContainer = document.querySelector('.project-container')
 let projectArray = [
     {
         number: 1,
-        imageUrl: "https://i.ytimg.com/vi/htGfnF1zN4g/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB5Cybcqqs_j2shlyK5ks1uj8Jv3g",
+        imageUrl: "https://i.postimg.cc/nzLbcJQZ/wt-lab-1.png",
         title: "Explore HTTP World",
-        description: "",
-        demoUrl: "./1-explore-http-world/lab1-github-api-fetch.html",
-
-        addedDate: dummyDate,
-        viewCount: 377,
-        commentCount: 100,
-        status: "Publish",
-        changeStatus: function () {
-            superChangeStatus(this)
-        },
-        addComment: function () {
-            superAddComment(this)
-        },
-        addView: function () {
-            superAddView(this)
-        }
-    }
+        description: "To get familiar with API, HTTP request and response to fetch github profile using username.",
+        documentation: "https://docs.google.com/document/d/1ASxivGWqHFIoxQ5YgiF0r3POel3PZFX6jj1bqSC7cBY/edit#heading=h.n9riga8o68dv",
+        demoUrl: "./1-explore-htttp-world/lab1-github-api-fetch.html",
+    },
+    {
+        number: 2,
+        imageUrl: "https://i.postimg.cc/T386jFBq/wt-lab-2.png",
+        title: "Responsive Web Design - freeCodeCamp Certification",
+        description: "To learn and implement basics of responsive web design and get 'Responsive Web Design' certification from freeCodeCamp.org.",
+        documentation: "https://docs.google.com/document/d/1ASxivGWqHFIoxQ5YgiF0r3POel3PZFX6jj1bqSC7cBY/edit#heading=h.r1eqtumyqaho",
+        demoUrl: "https://www.freecodecamp.org/certification/fcc68f23d5b-630e-4c16-b74f-a74685a59194/responsive-web-design",
+    },
+    {
+        number: 3,
+        imageUrl: "https://i.postimg.cc/nzLbcJQZ/wt-lab-1.png",
+        title: "Getting Started with Bootstrap",
+        description: "To learn to install and integrate Bootstrap into an existing project to make the project responsive.",
+        documentation: "https://docs.google.com/document/d/1ASxivGWqHFIoxQ5YgiF0r3POel3PZFX6jj1bqSC7cBY/edit#heading=h.ukmc52v7j7mz",
+        demoUrl: "./1-explore-htttp-world/lab1-github-api-fetch.html",
+    },
+    {
+        number: 4,
+        imageUrl: "https://i.postimg.cc/PrDGqKNL/wt-lab-4.png",
+        title: "Working with JavaScript Objects",
+        description: "To learn about JavaScript objects and map JavaScript objects with properties and methods into an HTML page..",
+        documentation: "https://docs.google.com/document/d/1ASxivGWqHFIoxQ5YgiF0r3POel3PZFX6jj1bqSC7cBY/edit#heading=h.hgall85br5t7",
+        demoUrl: "./4-javascript-object/lab4-js-object.html",
+    },
 ]
-function start() {
-    // initially initialize export component with empty string
-    let exportComponent = ``
-    // current index in the array initialize to 0
-    let index = 0
-    // loop for each array element
-    projectArray.forEach((item) => {
-        // append html with template literal to end of exportComponent as string
-        exportComponent += `
-        <div class="col-md-6 p-4">
-            <div class="blog-box" id="index-${index}">
-                <h3>${item.title}</h3>
-                <div class="inner-container">
-                    <div class="left">
-                        <a href="${item.refUrl}" target="_blank" class="image-box">
-                            <img src="${item.imageUrl}" alt="demo image">
-                        </a>
-                        </div>
-                        <div class="right data-box">
-                        <p>
-                            <strong>By:</strong> ${item.authorName}
-                        </p>
-                        <p>
-                            <strong>Added at:</strong> ${item.addedDate.getFullYear()}-${item.addedDate.getMonth()}-${item.addedDate.getDate()}
-                        </p>
-                        <p>
-                            <strong>Reads:</strong> ${item.viewCount}
-                        </p>
-                        <p>
-                            <strong>Comments:</strong> ${item.commentCount}
-                        </p>
-                        <p>
-                            <strong>Status:</strong> ${item.status}
-                        </p>
-                    </div>
+// initially initialize export component with empty string
+let exportComponent = ``
+// loop for each array element
+projectArray.forEach((item) => {
+    // append html with template literal to end of exportComponent as string
+    exportComponent += `
+        <div class="col-lg-4 col-md-6">
+            <div class="project-card card rounded-3">
+                <div class="img-box">
+                    <img src="${item.imageUrl}" alt="Image of the demo page">
                 </div>
-                <div class="button-box">
-                    <button class="button" onclick="projectArray[${index}].changeStatus()">Toggle:${item.status}</button>
-                    <button class="button" onclick="projectArray[${index}].addComment()">Add Comment</button>
-                    <button class="button" onclick="projectArray[${index}].addView()">Mark as Read</button>
+                <div class="detail-box p-md-4 p-2">
+                    <h2 class="project-title text-uppercase">
+                        Lab <span class="project-num">${item.number}</span>:
+                        ${item.title}
+                    </h2>
+                    <p class="project-description">
+                        ${item.description}
+                    </p>
+                    <div class="button-box">
+                        <a href="${item.documentation}" target="_blank">
+                            <div class="btn btn-outline-primary">
+                                <i class="fa-solid fa-book"></i>
+                                Documentation
+                            </div>
+                        </a>
+                        <a href="${item.demoUrl}" target="_blank">
+                            <div class="btn btn-primary">
+                                <i class="fa-solid fa-play"></i>
+                                Live Demo
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-        `
-        // increment index for next object in array
-        index++
-    })
-    // put the html string inside exportComponent as HTML inside blogContainer
-    blogContainer.innerHTML = exportComponent
-}
-
-start()
+    `
+})
+// put the html string inside exportComponent as HTML inside projectContainer
+projectContainer.innerHTML = exportComponent
